3f6591e56987b73ace1e5a61341c7db61f25342a
shayinhao <69226866+shayinhao@users.noreply.github.com>
2020-08-05 18:40:27 +0800

    Update SpringBootdemoApplication.java
    更新输出语句

--- SpringBootdemoApplication.java
+++ SpringBootdemoApplication.java
@@ -8,6 +8,7 @@ public class SpringBootdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootdemoApplication.class, args);
+        System.out.println("ahahaha");
     }
 
 }
